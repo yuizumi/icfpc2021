d944cf6dab3ed8ad2579b1d0c9c1ab2097cd6e5d
Yusuke Izumi <yuizumi@y5i.org>
2021-07-10 14:44:06 +0900

    Rewrite common.h

--- yuizumi/zero_getter.cc
+++ yuizumi/zero_getter.cc
@@ -4,42 +4,35 @@
 
 using namespace std;
 
-Solution Solve(const Problem& p)
+std::optional<Pose> Solve(const Problem& p)
 {
-    const int n = p.hole.size();
+    const int n = p.hole().size();
 
-    if (p.figure.vertices.size() != n) {
-        return Solution();
-    }
+    if (p.vertices().size() != n)
+        return std::nullopt;
 
     vector<int> map(n);
     for (int i = 0; i < n; i++) map[i] = i;
 
+    Pose pose(n);
     do {
-        const bool success = all_of(
-            p.figure.edges.begin(),
-            p.figure.edges.end(),
-            [&] (const Edge& e) {
-                const double d_orig = norm(p.figure.vertices[e.u] - p.figure.vertices[e.v]);
-                const double d_pose = norm(p.hole[map[e.u]] - p.hole[map[e.v]]);
-                return abs(d_pose - d_orig) * 1e+6 <= p.epsilon * d_orig;
-            }
-        );
-        if (success) {
-            Solution s;
-            s.reserve(n);
-            for (int i = 0; i < n; i++) s.push_back(p.hole[map[i]]);
-            return s;
-        }
+        for (int i = 0; i < n; i++) pose[i] = p.hole().vertices()[map[i]];
+        if (Validate(p, pose)) return pose;
     } while (next_permutation(map.begin(), map.end()));
 
-    return Solution();
+    return std::nullopt;
 }
 
 int main()
 {
     Json json;
     cin >> json;
-    cout << BuildJson(Solve(ParseJson(json))) << endl;
+
+    const Problem prob = Problem::FromJson(json);
+    const std::optional<Pose> pose = Solve(prob);
+    if (pose.has_value()) {
+        cout << ToJson(*pose) << endl;
+        cerr << "dislikes: " << Evaluate(prob, *pose) << endl;
+    }
     return 0;
 }
