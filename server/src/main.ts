626d132347d7f00f200f5919e83ed620c81a4f20
Yusuke Izumi <yuizumi@y5i.org>
2021-07-11 00:29:45 +0900

    Move visualizer out of tools

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,9 +8,9 @@ const port = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use("/tools", express.static(path.resolve(__dirname, "..", "..", "tools")));
 app.use("/problems", express.static(path.resolve(__dirname, "..", "..", "problems")));
 app.use("/solutions", express.static(path.resolve(__dirname, "..", "..", "solutions")));
+app.use("/visualizer", express.static(path.resolve(__dirname, "..", "..", "visualizer")));
 app.get("/", (req, res) => {
     res.send(`<!DOCTYPE html>
 <html>
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
     <meta charset="UTF-8" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
-    <script defer src="/tools/visualizer/dist/main.js"></script>
+    <script defer src="/visualizer/dist/main.js"></script>
 </head>
 <body>
     <div id="app"></div>
