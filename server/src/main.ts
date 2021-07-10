245b02e50d1d297808fd9dd2add0d957da21891d
Hiroshi Yamaguchi <sanberg12@gmail.com>
2021-07-10 17:22:01 +0900

    編集機能付きvisualizer

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,19 @@ app.use("/tools", express.static(path.resolve(__dirname, "..", "..", "tools")));
 app.use("/problems", express.static(path.resolve(__dirname, "..", "..", "problems")));
 app.use("/solutions", express.static(path.resolve(__dirname, "..", "..", "solutions")));
 app.get("/", (req, res) => {
-    res.send('<a href="/tools/visualizer.html">visualizer</a>');
+    res.send(`<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
+    <script defer src="/tools/dist/main.js"></script>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
+    `);
 });
 
 app.listen(port, () => {
