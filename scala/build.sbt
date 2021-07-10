34a51eaff770bcd6f86016d9a47df79c7034cfdc
Hiroshi Yamaguchi <sanberg12@gmail.com>
2021-07-10 09:19:24 +0900

    additional problems

--- scala/build.sbt
+++ scala/build.sbt
@@ -1,6 +1,9 @@
 lazy val root = project
   .in(file("."))
-  .aggregate(hello)
+  .aggregate(hello, rot3)
+  .settings(
+    name := "icfpc2021"
+  )
 
 lazy val hello = project
   .in(file("solutions/hello"))
@@ -23,5 +26,16 @@ lazy val rot3 = project
       "io.circe" %% "circe-core" % "0.14.1",
       "io.circe" %% "circe-generic" % "0.14.1",
       "io.circe" %% "circe-parser" % "0.14.1"
+    )
+  )
+
+lazy val download = project
+  .in(file("tools/download"))
+  .settings(
+    name := "download",
+    version := "0.1.0",
+    scalaVersion := "2.13.5",
+    libraryDependencies := Seq(
+      "com.softwaremill.sttp.client3" %% "core" % "3.3.9"
     )
   )
---
+++ scala/tools/download/src/main/scala/Download.scala
@@ -0,0 +1,24 @@
+import sttp.client3._
+
+import java.io.{File, PrintWriter}
+
+object Download {
+  def main(args: Array[String]): Unit = {
+    for {
+      idx <- 1 to 78
+    } {
+      println(idx)
+      val request = basicRequest
+        .header("Authorization", s"Bearer ${sys.env("ICFPC2021_APIKEY")}")
+        .get(uri"https://poses.live/api/problems/$idx")
+      val backend = HttpURLConnectionBackend()
+      val response = request.send(backend)
+      response.body.foreach { body =>
+        val pw = new PrintWriter(new File(String.format("../problems/%03d.problem", idx)))
+        pw.print(body)
+        pw.flush()
+        pw.close()
+      }
+    }
+  }
+}
