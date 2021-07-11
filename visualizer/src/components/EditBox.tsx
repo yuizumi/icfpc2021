acc345d30f3ed57cf8de83db6a078e01e1ef3e5d
Hiroshi Yamaguchi <sanberg12@gmail.com>
2021-07-11 15:51:16 +0900

    手で直すと反映されなくなるのを修正

--- visualizer/src/components/EditBox.tsx
+++ visualizer/src/components/EditBox.tsx
@@ -8,30 +8,52 @@ export type Props = {
     onInit: () => void;
 };
 
-export const EditBox = (props: Props) => {
-    return (
-        <form
-            onSubmit={e => {
-                e.preventDefault();
-                props.onEdit((e.target as EventTarget & { solution: { value: string; }; }).solution.value);
-            }}
-            onReset={e => {
-                props.onClear();
-            }}
-        >
-            <div>
-                <textarea
-                    name="solution"
-                    cols={80}
-                    rows={20}
-                    defaultValue={props.solution != null ? JSON.stringify(props.solution, undefined, 2) : ""}
-                />
-            </div>
-            <div>
-                <input type="submit" />
-                <input type="reset" />
-                <button onClick={() => props.onInit()}>初期ポーズを読み込む</button>
-            </div>
-        </form>
-    );
+export type State = {
+    solution?: string;
+    viewSolution?: string;
 };
+
+export class EditBox extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            solution: props.solution != null ? JSON.stringify(props.solution, undefined, 2) : "",
+            viewSolution: props.solution != null ? JSON.stringify(props.solution, undefined, 2) : ""
+        };
+    }
+
+    render() {
+        const s = this.props.solution != null ? JSON.stringify(this.props.solution, undefined, 2) : "";
+        if (s !== this.state.solution) {
+            this.setState({ solution: s, viewSolution: s });
+        }
+        return (
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    this.props.onEdit((e.target as EventTarget & { solution: { value: string; }; }).solution.value);
+                }}
+                onReset={e => {
+                    this.props.onClear();
+                }}
+            >
+                <div>
+                    <textarea
+                        name="solution"
+                        cols={80}
+                        rows={20}
+                        value={this.state.viewSolution}
+                        onChange={e => {
+                            this.setState({ viewSolution: e.target.value })
+                        }}
+                    />
+                </div>
+                <div>
+                    <input type="submit" />
+                    <input type="reset" />
+                    <button onClick={() => this.props.onInit()}>初期ポーズを読み込む</button>
+                </div>
+            </form>
+        );
+    };
+}
